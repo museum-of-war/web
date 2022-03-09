d2d8715df26026c74f26170266ee7b785adfa8a5
Isaac Kamlish <isaac.k@hotmail.co.uk>
2022-03-09 22:25:57 +0000

    content top trial

--- sections/Content/ContentTop.tsx
+++ sections/Content/ContentTop.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ContentTop = () => {
-  return <div>ContentTop</div>;
+  return (
+    <div className="flex w-30%">
+      <img alt="Logo" src={"img/logo.png"} width="30%" />
+      <div className="flex flex-row font-rlight">
+        <div>An NFT-museum of the war of putin's russia against Ukraine</div>
+        <div className="text-sm	" style = {{color:'yellow'}}> NFT-музей війни путінської росії проти України. </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContentTop;
