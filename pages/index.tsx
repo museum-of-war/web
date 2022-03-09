3417c9c2c728d4b9db668bbd59a6f05ef506037b
natecoh <nate@frame.xyz>
2022-03-09 21:58:26 +0000

    Added fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-3xl font-bold">We are live</div>;
+  return <div className="font-rnarrow">We are live</div>;
 };
 
 export default Home;
