8bfbba70acfed68e28156ca694ab2473ac1630f4
natecoh <nate@frame.xyz>
2022-03-09 23:45:52 +0000

    Fixed styles

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-row px-10% font-rnarrow">
+      <p className="">© 2022 Copyright</p>
+    </div>
+  );
 };
 
 export default Footer;
