0fc6b50fc4b00ea2e9ac43107b1652877115bb15
natecoh <nate@frame.xyz>
2022-03-10 14:56:28 +0000

    Set up wrapper for the app

---
+++ hooks/useAppRouter.ts
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router'
+import { ParsedUrlQuery } from 'querystring'
+
+type HookReturnType<QueryParams extends any> = ReturnType<typeof useRouter> &
+  (
+    | { isReady: true; query: QueryParams }
+    | { isReady: false; query: Record<keyof QueryParams, undefined> }
+  )
+
+export const useAppRouter = <
+  QueryParams extends ParsedUrlQuery = ParsedUrlQuery,
+>(): HookReturnType<QueryParams> => {
+  const router = useRouter()
+
+  return { ...router, query: router.query } as HookReturnType<QueryParams>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,10 @@
-import { useWeb3Modal } from "@hooks/useWeb3Modal";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-import Content from "../sections/Content/Content";
-import Footer from "../sections/Footer/Footer";
-import Header from "../sections/Header/Header";
+import AboutProject from "../sections/AboutProject/AboutProject";
 
 const Home: NextPage = () => {
-  const [signerAddress, setSignerAddress] = useState<string>("");
-  const { provider, connectWallet, disconnectWallet } = useWeb3Modal();
-
-  const handleConnect = async () => {
-    await connectWallet();
-  };
-  const handleDisconnect = () => {
-    disconnectWallet();
-  };
-
-  useEffect(() => {
-    const getAddress = async () => {
-      if (provider !== undefined) {
-        const signer = provider.getSigner();
-        const address = await signer.getAddress();
-        setSignerAddress(address);
-      } else {
-        setSignerAddress("");
-      }
-    };
-    getAddress();
-  }, [provider]);
   return (
     <div className="text-carbon">
-      <Header
-        signerAddress={signerAddress}
-        handleConnect={handleConnect}
-        handleDisconnect={handleDisconnect}
-      />
-      <Content />
-      <Footer />
+      <AboutProject />
     </div>
   );
 };
