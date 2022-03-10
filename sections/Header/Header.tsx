bdc5bd3f7d12e390e866b1d324d65eeea1d45f19
natecoh <nate@frame.xyz>
2022-03-10 00:55:43 +0000

    Designed header and footer for mobile

--- sections/Header/Header.tsx
+++ sections/Header/Header.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ConnectWalletButton from "../../components/ConnectWalletButton";
 import HeaderAndFooterButton from "../../components/HeaderAndFooterButton";
+import { isMobile } from "react-device-detect";
 
 const Header = () => {
   return (
-    <div className="flex flex-row w-screen px-10% items-center mb-8% pt-2%">
+    <div className="flex flex-row w-screen tablet:pl-10% tablet:pr-13% mobile:px-10% items-center mb-8% pt-2% justify-between">
       <img
-        className="w-10% min-w-75px laptop:mr-30% tablet:mr-25% mobile:mr-15%"
-        src={"/img/logo.png"}
+        className="w-15% min-w-75px laptop:mr-30% tablet:mr-25% mobile:mr-15%"
+        src={"/img/pd-logoNoSymbol.png"}
         alt="Logo"
       />
-      <HeaderAndFooterButton label="Warline" onClick={() => {}} />
-      <HeaderAndFooterButton label="About project" onClick={() => {}} />
-      <HeaderAndFooterButton label="My tokens" onClick={() => {}} />
-      <ConnectWalletButton />
+      {!isMobile && (
+        <>
+          <HeaderAndFooterButton label="Warline" onClick={() => {}} />
+          <HeaderAndFooterButton label="About project" onClick={() => {}} />
+          <HeaderAndFooterButton label="My tokens" onClick={() => {}} />
+          <ConnectWalletButton />
+        </>
+      )}
+      {isMobile && <HeaderAndFooterButton label="Menu" onClick={() => {}} />}
     </div>
   );
 };
