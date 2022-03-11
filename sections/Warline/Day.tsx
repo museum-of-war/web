a0f040869bfadbb6b92533db752bde908a88ae13
Isaac Kamlish <isaac.k@hotmail.co.uk>
2022-03-11 00:31:26 +0000

    warline page completed

--- sections/Warline/Day.tsx
+++ sections/Warline/Day.tsx
@@ -1,20 +1,58 @@
 import { DayType } from "@sections/types";
 import React from "react";
 import Event from "./Event";
+import { useViewPort } from "@hooks/useViewport";
 
 type PropsDay = {
   dayData: DayType;
 };
 
 const Day = ({ dayData }: PropsDay) => {
-  return (
-    <div className="flex flex-row">
+  const { isMobile, isTablet } = useViewPort();
+  return isMobile ? (
+    <div className="mb-40px flex flex-col">
+      <div className="w-100%">
+        <div className=" flex flex-row justify-between font-rblack items-top">
+          <p className="mt-20px text-16px font-rnarrow">{dayData.date}</p>
+          <div className=" flex flex-row">
+            <p className="text-32px mr-20px">Day</p>
+            <p className="text-70px leading-70px">{dayData.dayNo}</p>
+          </div>
+        </div>
+        <div className="mt-1px mb-20px h-5px w-100% bg-carbon"></div>
+      </div>
+      <div className=" w-100%">
+        {dayData.events.map((eventData, idx) => (
+          <Event eventData={eventData} />
+        ))}
+      </div>
+    </div>
+  ) : isTablet ? (
+    <div className="mb-40px flex flex-col">
+      <div className="w-100%">
+        <div className=" flex flex-row justify-between font-rblack items-top">
+          <p className="mt-20px text-16px font-rnarrow">{dayData.date}</p>
+          <div className=" flex flex-row">
+            <p className="text-32px mr-20px">Day</p>
+            <p className="text-70px leading-70px">{dayData.dayNo}</p>
+          </div>
+        </div>
+        <div className="mt-1px mb-20px h-5px w-100% bg-carbon"></div>
+      </div>
+      <div className=" w-100%">
+        {dayData.events.map((eventData, idx) => (
+          <Event eventData={eventData} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="mb-40px flex flex-row">
       <div className="w-30%">
         <div className=" flex flex-row justify-between font-rblack items-top">
           <p className="text-32px">Day</p>
           <p className="text-70px leading-70px">{dayData.dayNo}</p>
         </div>
-        <div className="h-5px w-100% bg-carbon"></div>
+        <div className="mt-1px h-5px w-100% bg-carbon"></div>
         <p className="mt-20px text-16px font-rnarrow">{dayData.date}</p>
       </div>
       <div className="ml-5% w-70%">
--- sections/Warline/Event.tsx
+++ sections/Warline/Event.tsx
@@ -1,16 +1,72 @@
 import React from "react";
+import { useViewPort } from "@hooks/useViewport";
+import { EventType } from "@sections/types";
 
 type PropsEvent = {
   eventData: EventType;
 };
 
 const Event = ({ eventData }: PropsEvent) => {
-  return (
-    <div className="flex flex-row items-top mb-50px">
+  const { isMobile, isTablet } = useViewPort();
+
+  return isMobile ? (
+    <div className="flex flex-col items-top mb-60px">
+      <img alt="Logo" src={eventData.imageUrl} className="w-100%" />
+      <div className="mt-20px flex flex-col justify-between">
+        <div>
+          <div className="flex flex-row items-center justify-between ">
+            <p className="font-rblack leading-32px text-32px">
+              {eventData.time}
+            </p>
+            <p className="font-rlight">#000{eventData.tokenId}</p>
+          </div>
+          <p className="font-rnarrow pt-15px"> {eventData.description}</p>
+          <div className="flex flex-row items-center justify-between pt-5px">
+            <p className="font-rlight ">{eventData.username}</p>
+            <img
+              alt="Logo"
+              src={"img/warline-TwitterLogo.png"}
+              className="w-50px"
+            />
+          </div>
+        </div>
+        <p className="font-rblack mt-15px ">See Details</p>
+      </div>
+    </div>
+  ) : isTablet ? (
+    <div className="flex flex-row items-top mb-60px">
       <img
         alt="Logo"
         src={eventData.imageUrl}
-        className="w-20vw h-20vw mr-10%"
+        className="w-40vw max-w-300px max-h-300px h-40vw mr-50px"
+      />
+      <div className="flex flex-col justify-between">
+        <div>
+          <div className="flex flex-row items-center justify-between ">
+            <p className="font-rblack leading-32px text-32px">
+              {eventData.time}
+            </p>
+            <p className="font-rlight">#000{eventData.tokenId}</p>
+          </div>
+          <p className="font-rnarrow pt-15px"> {eventData.description}</p>
+          <div className="flex flex-row items-center justify-between pt-15px">
+            <p className="font-rlight ">{eventData.username}</p>
+            <img
+              alt="Logo"
+              src={"img/warline-TwitterLogo.png"}
+              className="w-50px"
+            />
+          </div>
+        </div>
+        <p className="font-rblack ">See Details</p>
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-row items-top mb-60px">
+      <img
+        alt="Logo"
+        src={eventData.imageUrl}
+        className="w-20vw max-w-300px max-h-300px h-20vw mr-50px"
       />
       <div className="flex flex-col justify-between">
         <div>
