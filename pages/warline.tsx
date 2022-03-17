4738b379cf58d8df86321c12d49ca591a1ac2e82
natecoh <nate@frame.xyz>
2022-03-17 23:09:11 +0000

    Fixed warline issue

--- pages/warline.tsx
+++ pages/warline.tsx
@@ -1,13 +1,8 @@
 import { SharedProps } from "@components/wrapper";
-import { useAppRouter } from "@hooks/useAppRouter";
 import type { NextPage } from "next";
 import Warline from "../sections/Warline/Warline";
 
 const WarlinePage: NextPage<SharedProps> = (props) => {
-  const { push } = useAppRouter();
-  if (!props.signerAddress) {
-    push("/");
-  }
   return <Warline />;
 };
 
