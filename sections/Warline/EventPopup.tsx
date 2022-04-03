2266b4ea831c96d46718dfb7fcc588296123cd00
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-03 14:22:35 +0300

    Details modal fixes

--- sections/Warline/EventPopup.tsx
+++ sections/Warline/EventPopup.tsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import FsLightbox from "fslightbox-react";
-import { useViewPort } from "@hooks/useViewport";
-import { EventType } from "@sections/types";
-import { VscChromeClose } from "react-icons/vsc";
-import { openInNewTab } from "@sections/utils";
-import { getUrls } from "@sections/Warline/WarlineUrls";
+import {useViewPort} from "@hooks/useViewport";
+import {EventType} from "@sections/types";
+import {VscChromeClose} from "react-icons/vsc";
+import {openInNewTab} from "@sections/utils";
+import {getUrls} from "@sections/Warline/WarlineUrls";
+import {usePopup} from "../../providers/PopupProvider";
 
 type PropsPopup = {
   eventData: EventType;
   dayNo: number;
-  date: string;
-  setShowPopup: (arg: boolean) => void;
   idx: number;
-  eventsData: EventType[];
   allEvents: EventType[];
 };
 
@@ -30,16 +28,14 @@ const rand_imgs = [
 const Popup = ({
   eventData,
   dayNo,
-  date,
-  setShowPopup,
   idx,
-  eventsData,
   allEvents
 }: PropsPopup) => {
   const [data, setData] = useState<EventType>(eventData);
   const [currIdx, setCurrIdx] = useState<number>(() => {
     return allEvents.filter(event => Number(event.DayNo) < dayNo).length + idx;
   });
+  const {hidePopup} = usePopup();
   const [toggler, setToggler] = useState<boolean>(false);
 
   const alt = useMemo(() => {
@@ -50,23 +46,22 @@ const Popup = ({
     setData(allEvents[currIdx] as EventType);
   }, [currIdx]);
 
-
-  const { isMobile, isTablet } = useViewPort();
+  const {isMobile, isTablet} = useViewPort();
 
   const TokenidFormatter = (tokenId: string) => {
     return parseInt(tokenId) < 10
       ? "#000" + tokenId
       : parseInt(tokenId) < 100
-      ? "#00" + tokenId
-      : parseInt(tokenId) < 1000
-      ? "#0" + tokenId
-      : "#" + tokenId;
+        ? "#00" + tokenId
+        : parseInt(tokenId) < 1000
+          ? "#0" + tokenId
+          : "#" + tokenId;
   };
 
   const renderPrevBtn = (className: string = "", imgClass: string = "") => (
     <div className={`z-20 ${className}`}>
       <button onClick={() => currIdx > 0 && setCurrIdx(currIdx - 1)}>
-        <img src={"img/modal-left.svg"} className={imgClass} />
+        <img src={"img/modal-left.svg"} className={imgClass}/>
       </button>
     </div>
   );
@@ -78,7 +73,7 @@ const Popup = ({
       >
         <img
           src={"img/modal-left.svg"}
-          style={{ transform: " scale(-1, 1)" }}
+          style={{transform: " scale(-1, 1)"}}
           className={imgClass}
         />
       </button>
@@ -103,7 +98,7 @@ const Popup = ({
           onClick={() => setToggler(!toggler)}
           src={logoSrc}
           className={`${className} hover:cursor-pointer`}
-          onError={({ currentTarget }) => {
+          onError={({currentTarget}) => {
             if (isAnimation) {
               currentTarget.src = previewSrc;
               currentTarget.onerror = () => {
@@ -124,80 +119,17 @@ const Popup = ({
     );
   };
 
-  return isMobile ? (
-    <div>
-      <div className="absolute -mt-50% z-20 w-screen100% min-h-screen90% bg-white left-0 flex flex-row px-7% py-10%	">
-        <button
-          className="absolute right-20px top-20px"
-          onClick={() => setShowPopup(false)}
-        >
-          <VscChromeClose size={25} />
-        </button>
-        <div className="w-100%">
-          <div className="flex flex-col items-start justify-between ">
-            <p className="font-rblack leading-32px text-32px">
-              Day {data.DayNo}, {data.Time}
-            </p>
-            <p className="font-rlight text-24px pt-20px">
-              {TokenidFormatter(data.Tokenid)}
-            </p>
-          </div>
-          <div className="flex flex-col items-start justify-between">
-            <div className="pt-20px">
-              <p className="font-rlight text-16px">Artist</p>
-              <button
-                className="pt-5px font-rnarrow text-18px"
-                onClick={() => {
-                  if (data.ArtistLink) openInNewTab(data.ArtistLink);
-                }}
-              >
-                <p>{data.ArtistName}</p>
-              </button>
-            </div>
-          </div>
-          {renderImg()}
-          <div className="bg-beige px-3% py-2% mt-8%">
-            <p className="font-rnarrow pt-15px"> {data.Headline}</p>
-            <div className="flex flex-row items-center justify-between pt-15px">
-              <p className="font-rlight ">@{data.TwitterUsername}</p>
-              <button
-                onClick={() => {
-                  openInNewTab(data.TwitterUrl);
-                }}
-              >
-                <img
-                  alt="Twitter"
-                  src={"img/warline-TwitterLogo.png"}
-                  className="w-50px"
-                />
-              </button>
-            </div>
-          </div>
-          <p className="font-rlight text-24px mt-8%">Description</p>
-
-          <p className="pt-2% font-rnarrow text-18px">
-            {data.DescriptionEnglish}
-          </p>
-          <p className="pt-5% font-rnarrow text-18px">
-            {data.DescriptionUkrainian}
-          </p>
+  return isMobile || isTablet ? (
+    <div className="fixed z-10 w-screen100% h-screen100% top-0 left-0 flex items-center justify-evenly">
+      <div className="z-20 bg-white absolute w-100% flex flex-col px-5% py-3% overflow-auto top-0" style={{ height: 'calc(100% - 60px)' }}>
+        <div className="w-100% text-right">
+          <button
+            className="right-20px top-20px"
+            onClick={hidePopup}
+          >
+            <VscChromeClose size={25}/>
+          </button>
         </div>
-      </div>
-
-      <div className="fixed z-10 w-screen100% h-screen100% bg-carbon top-0 left-0 opacity-70	"></div>
-    </div>
-  ) : isTablet ? (
-    <>
-      {renderPrevBtn("z-20 fixed left-3% top-50%", "w-75%")}
-      {renderNextBtn("z-20 fixed right-3% top-50%", "w-75%")}
-
-      <div className="z-20 bg-white absolute flex flex-row px-7% left-10% right-8% w-80% py-10%">
-        <button
-          className="absolute right-20px top-20px"
-          onClick={() => setShowPopup(false)}
-        >
-          <VscChromeClose size={25} />
-        </button>
         <div className="w-100%">
           <div className="flex flex-row items-center justify-between ">
             <p className="font-rblack leading-32px text-32px">
@@ -247,18 +179,25 @@ const Popup = ({
           </p>
         </div>
       </div>
-      <div className="fixed z-10 w-screen100% h-screen100% bg-carbon top-0 left-0 opacity-70	"></div>
-    </>
+      <div className="fixed bottom-0 bg-black left-0 right-0 z-20 flex items-center justify-evenly h-60px">
+        {renderPrevBtn("", "w-75%")}
+        {renderNextBtn("", "w-75%")}
+      </div>
+    </div>
   ) : (
-    <div className="fixed z-10 w-screen100% h-screen100% top-0 left-0 flex  items-center justify-evenly">
-      {renderPrevBtn()}
+    <div className="fixed z-10 w-screen100% h-screen100% top-0 left-0 flex items-center justify-evenly"
+         onClick={(e) => {
+           if (e.target !== e.currentTarget) return;
 
-      <div className="z-20 bg-white relative w-80% flex flex-row px-5% py-3%	">
+           hidePopup();
+         }}>
+      {renderPrevBtn()}
+      <div className="z-20 h-4/6 bg-white relative w-80% flex flex-row px-5% py-3% overflow-auto">
         <button
           className="absolute right-20px top-20px"
-          onClick={() => setShowPopup(false)}
+          onClick={hidePopup}
         >
-          <VscChromeClose size={25} />
+          <VscChromeClose size={25}/>
         </button>
         <div className="w-35%">{renderImg("w-90% pr-5%")}</div>
         <div className="w-65%">
@@ -283,7 +222,6 @@ const Popup = ({
               </button>
             </div>
           </div>
-
           <div className="bg-beige px-3% py-2% mt-5%">
             <p className="font-rnarrow pt-15px"> {data.Headline}</p>
             <div className="flex flex-row items-center justify-between pt-15px">
@@ -311,8 +249,6 @@ const Popup = ({
         </div>
       </div>
       {renderNextBtn()}
-
-      <div className=" fixed z-10 w-screen100% h-screen100% bg-carbon top-0 left-0 opacity-70"></div>
     </div>
   );
 };
