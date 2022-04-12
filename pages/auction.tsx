da3c3f576d44f442a0c3b9a7c42acdc1c553206b
Vadim <eugene.s@zpoken.ai>
2022-04-12 19:30:13 +0300

    create auction page

---
+++ pages/auction.tsx
@@ -0,0 +1,20 @@
+import { SharedProps } from "@components/wrapper";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const AuctionPage: NextPage<SharedProps> = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Auction - Meta History: Museum of War</title>
+        <meta
+          name="description"
+          content="Auction - Meta History: Museum of War"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default AuctionPage;
