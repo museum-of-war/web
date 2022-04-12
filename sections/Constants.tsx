da1b971c8e7a9171ed83665e122b1382b5d573d7
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-12 18:59:57 +0300

    Progress #42

--- sections/Constants.tsx
+++ sections/Constants.tsx
@@ -1,10 +1,12 @@
 export const TWITTER_LINK =
-  "https://mobile.twitter.com/Meta_History_UA?t=gjhrJDPwV026--t4UsgkYA&s=09";
+  'https://mobile.twitter.com/Meta_History_UA?t=gjhrJDPwV026--t4UsgkYA&s=09';
+export const TWITTER_LINK_MOBILE =
+  'https://twitter.com/Meta_History_UA?t=gjhrJDPwV026--t4UsgkYA&s=09';
 export const INSTAGRAM_LINK =
-  "https://instagram.com/meta_history_ua?utm_medium=copy_link";
-export const GITHUB_LINK = "https://github.com/museum-of-war";
-export const OPENSEA_LINK = "https://opensea.io/collection/metahistorymuseum";
-export const FRAME_LINK = "https://www.fair.xyz";
-export const MINISTRY_LINK = "https://thedigital.gov.ua/";
+  'https://instagram.com/meta_history_ua?utm_medium=copy_link';
+export const GITHUB_LINK = 'https://github.com/museum-of-war';
+export const OPENSEA_LINK = 'https://opensea.io/collection/metahistorymuseum';
+export const FRAME_LINK = 'https://www.fair.xyz';
+export const MINISTRY_LINK = 'https://thedigital.gov.ua/';
 export const RELEASE_DATE = `2022-03-30T16:59:59.000-04:00`;
 export const MINT_LINK = `https://app.fair.xyz/collection/MHXYZ/queue`;
