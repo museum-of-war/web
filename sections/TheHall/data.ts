6774b13d8d20ed9d84d4d46fad176801a77da301
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-12 19:10:58 +0300

    Progress #42

---
+++ sections/TheHall/data.ts
@@ -0,0 +1,34 @@
+import { HallItemType } from '@sections/types';
+
+export const data: HallItemType[] = [
+  {
+    Id: 1,
+    TwitterUrl: 'https://twitter.com/asd',
+    ImageSrc:
+      'https://upload.wikimedia.org/wikipedia/uk/a/a8/Kiev_Independence_Square_cropped.jpg',
+    TwitterUsername: 'elonmusk',
+    TwitterAccountName: 'Elon Musk',
+    TweetText: `Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ea, harum laboriosam maxime minus nulla officiis
+              pariatur porro qui veritatis voluptatibus voluptatum? A ad alias amet architecto atque cupiditate delectus,
+              deleniti ducimus est iure modi mollitia nostrum rem similique sit totam voluptatem! Atque delectus distinctio
+              earum harum magnam neque nihil officiis provident quas voluptatum. Alias amet architecto dignissimos earum, id
+              incidunt maiores nam neque optio, praesentium provident quia ratione recusandae tempore temporibus totam unde
+              voluptates! Adipisci alias fugiat quaerat quidem!`,
+  },
+  {
+    Id: 2,
+    TwitterUrl: 'https://twitter.com/asd',
+    ImageSrc:
+      'https://upload.wikimedia.org/wikipedia/uk/a/a8/Kiev_Independence_Square_cropped.jpg',
+    TwitterUsername: 'elonmusk',
+    TwitterAccountName: 'Elon Musk',
+    TweetText: `Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ea, harum laboriosam maxime minus nulla officiis
+              pariatur porro qui veritatis voluptatibus voluptatum? A ad alias amet architecto atque cupiditate delectus,
+              deleniti ducimus est iure modi mollitia nostrum rem similique sit totam voluptatem! Atque delectus distinctio
+              earum harum magnam neque nihil officiis provident quas voluptatum. Alias amet architecto dignissimos earum, id
+              incidunt maiores nam neque optio, praesentium provident quia ratione recusandae tempore temporibus totam unde
+              voluptates! Adipisci alias fugiat quaerat quidem!`,
+  },
+];
