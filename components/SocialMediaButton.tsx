0e1a4e7f11b5c8eaed2cd328ca93915280e23e84
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-12 18:59:06 +0300

    Progress #42

--- components/SocialMediaButton.tsx
+++ components/SocialMediaButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 type SocialMediaButtonProps = {
   twitter?: boolean;
@@ -18,16 +18,14 @@ const SocialMediaButton = ({
   customClasses = '',
 }: SocialMediaButtonProps) => {
   return (
-    <div className={`mobile:w-40px w-48px mobile:mr-6% tablet:mr-24px last:mr-0 ${customClasses}`}>
-      <a
-        href={href}
-        target="_blank"
-        rel="noreferrer"
-      >
-        {twitter && <img alt="Twitter Logo" src={"/img/pd-TwitterLogo.svg"} />}
-        {instagram && <img alt="Instagram Logo" src={"/img/pd-IGLogo.svg"} />}
-        {github && <img alt="GitHub Logo" src={"/img/gh.svg"} />}
-        {opensea && <img alt="OpenSea Logo" src={"/img/opensea.svg"} />}
+    <div
+      className={`mobile:w-40px w-48px mobile:mr-6% tablet:mr-24px last:mr-0 ${customClasses}`}
+    >
+      <a href={href} target="_blank" rel="noreferrer">
+        {twitter && <img alt="Twitter Logo" src={'/img/pd-TwitterLogo.svg'} />}
+        {instagram && <img alt="Instagram Logo" src={'/img/pd-IGLogo.svg'} />}
+        {github && <img alt="GitHub Logo" src={'/img/gh.svg'} />}
+        {opensea && <img alt="OpenSea Logo" src={'/img/opensea.svg'} />}
       </a>
     </div>
   );
