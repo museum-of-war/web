e8f79442d3cf269813a018065fb663d10d3b00b8
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-13 15:08:08 +0300

    Progress #42

--- components/SocialMediaButton.tsx
+++ components/SocialMediaButton.tsx
@@ -24,9 +24,7 @@ const SocialMediaButton = ({
       className={`mobile:w-40px w-48px mobile:mr-6% tablet:mr-24px last:mr-0 ${customClasses}`}
     >
       <a href={href} target="_blank" rel="noreferrer">
-        {telegram && (
-          <img alt="Telegram Logo" src={'/img/pd-TwitterLogo.svg'} />
-        )}
+        {telegram && <img alt="Telegram Logo" src={'/img/tg.svg'} />}
         {twitter && <img alt="Twitter Logo" src={'/img/pd-TwitterLogo.svg'} />}
         {instagram && <img alt="Instagram Logo" src={'/img/pd-IGLogo.svg'} />}
         {github && <img alt="GitHub Logo" src={'/img/gh.svg'} />}
