a16b1f6f8c97ebccd0dc2b1c9b62dabf61c9cc36
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-04-13 14:01:52 +0300

    Progress #42

--- components/SocialMediaButton.tsx
+++ components/SocialMediaButton.tsx
@@ -5,6 +5,7 @@ type SocialMediaButtonProps = {
   instagram?: boolean;
   github?: boolean;
   opensea?: boolean;
+  telegram?: boolean;
   href: string;
   customClasses?: string;
 };
@@ -15,6 +16,7 @@ const SocialMediaButton = ({
   github,
   opensea,
   href,
+  telegram,
   customClasses = '',
 }: SocialMediaButtonProps) => {
   return (
@@ -22,6 +24,9 @@ const SocialMediaButton = ({
       className={`mobile:w-40px w-48px mobile:mr-6% tablet:mr-24px last:mr-0 ${customClasses}`}
     >
       <a href={href} target="_blank" rel="noreferrer">
+        {telegram && (
+          <img alt="Telegram Logo" src={'/img/pd-TwitterLogo.svg'} />
+        )}
         {twitter && <img alt="Twitter Logo" src={'/img/pd-TwitterLogo.svg'} />}
         {instagram && <img alt="Instagram Logo" src={'/img/pd-IGLogo.svg'} />}
         {github && <img alt="GitHub Logo" src={'/img/gh.svg'} />}
