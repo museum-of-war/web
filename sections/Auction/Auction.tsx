a195230b526b7b86be0306b9c9ed5e7a8f060f3e
Vadim <eugene.s@zpoken.ai>
2022-04-17 16:09:11 +0300

    change drop nft logic

--- sections/Auction/Auction.tsx
+++ sections/Auction/Auction.tsx
@@ -1,8 +1,10 @@
 import Blurb from "@sections/AboutProject/Blurb";
-import { ethers } from "ethers";
-import { NFTAuctionConnect } from "@museum-of-war/auction";
 import ContentAuction from "./ContentAuction";
 import DropNft from "./DropNft";
+import { useWeb3Modal } from "@hooks/useWeb3Modal";
+import { useEffect, useState } from "react";
+import { openInNewTab } from "@sections/utils";
+import { MINT_LINK, OPENSEA_LINK } from "@sections/Constants";
 
 type AuctionProps = {
   signerAddress: string;
@@ -10,6 +12,16 @@ type AuctionProps = {
 };
 
 const Auction = ({ signerAddress, handleConnect }: AuctionProps) => {
+  const [isCanMint, setCanMint] = useState<boolean>(false);
+
+  const { canMint } = useWeb3Modal();
+
+  useEffect(() => {
+    canMint().then((i) => setCanMint(i));
+  }, []);
+
+  const handleBuyNft = () => openInNewTab(isCanMint ? MINT_LINK : OPENSEA_LINK);
+
   return (
     <>
       <Blurb
@@ -18,16 +30,15 @@ const Auction = ({ signerAddress, handleConnect }: AuctionProps) => {
         ukrainian="Обмежений набір рідкісних та відібраних робіт українських та світових художників. Плаваюча ціна, що базується на ставках та пропозиціях. На цей раз Музей запрошує колекціонерів мистецтва повністю сформувати благодійний збір коштів. Зверніть увагу, що лише колекціонери експонатів Warline можуть брати участь в аукціоні."
       />
       <ContentAuction />
-      <DropNft
-        desc="Meanwhile, the current drop is still on sale. The NFTs are unique but any NFT will support Ukraine. Get yours."
-        buttonLabel="Buy NFT Now"
-        className="tablet:my-72px mobile:my-40px"
-      />
-      <DropNft
-        desc="When you're waiting for new arts, you can still donate to Ukraine right away."
-        buttonLabel="Support Ukraine"
-        className="laptop:mb-120px tablet:mb-96px mobile:mb-[60px]"
-      />
+
+      {isCanMint && (
+        <DropNft
+          desc="Meanwhile, the current drop is still on sale. The NFTs are unique but any NFT will support Ukraine. Get yours."
+          buttonLabel="Buy NFT Now"
+          className="laptop:mb-120px tablet:mb-96px mobile:mb-[60px] tablet:my-72px mobile:my-40px"
+          handleClick={handleBuyNft}
+        />
+      )}
     </>
   );
 };
