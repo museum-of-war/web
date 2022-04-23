e99a01183037e31906c44aa9146db7553fc33244
Danylo Mysak <danmysak@gmail.com>
2022-04-23 18:26:48 +0300

    Structured data: logo

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,19 +1,19 @@
+import { Thing, WithContext } from 'schema-dts';
 import Head from 'next/head';
 
 type PageHeadProps = {
     title: string;
+    data?: WithContext<Thing>;
 };
 
 function PageHead({
-  title
+  title,
+  data,
 }: PageHeadProps) {
     return (
         <Head>
             <title>{title}</title>
-            <meta
-                name="description"
-                content={title}
-            />
+            <meta name="description" content={title} />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
@@ -22,6 +22,11 @@ function PageHead({
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
+            { data && (
+                <script type="application/ld+json">
+                    { JSON.stringify(data) }
+                </script>
+            ) }
         </Head>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,15 @@ import AboutProject from '../sections/AboutProject/AboutProject';
 const Home: NextPage<SharedProps> = (props) => {
   return (
     <>
-      <PageHead title="About - Meta History: Museum of War" />
+      <PageHead
+          title="About - Meta History: Museum of War"
+          data={{
+              '@context': 'https://schema.org',
+              '@type': 'Organization',
+              url: location.origin,
+              logo: `${location.origin}/logo.svg`,
+          }}
+      />
       <AboutProject
         signerAddress={props.signerAddress}
         handleConnect={props.handleConnect}
