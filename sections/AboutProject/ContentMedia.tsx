8bf4b9cd3a8766e3e885084642a17fcb8286a78b
Oleksandr Krat <oleksandrkrat@gmail.com>
2022-04-24 19:29:18 +0300

    Changed to grid

--- sections/AboutProject/ContentMedia.tsx
+++ sections/AboutProject/ContentMedia.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Media = (summary: string, outlet: string, url: string) => {
   return (
-    <div className="tablet:pr-10%">
+    <div>
       <p className="font-rnarrow mobile:text-18px laptop:text-16px desktop:text-18px">
         {summary}
       </p>
@@ -27,38 +27,34 @@ const ContentMedia = () => {
         IN PRESS
       </p>
       <div className="h-5px w-100% bg-carbon"></div>
-      <div className="mt-20px tablet:flex tablet:flex-row font-rlight justify-between">
-        <div className="tablet:flex laptop:flex-row tablet:flex-col">
-          <div className="mr-3%">
-            {Media(
-              "Ukraine's government has said it will issue non-fungible tokens (NFTs) to fund its military as they defend the country against the Russian invasion.",
-              "BBC",
-              "https://www.bbc.co.uk/news/business-60613613"
-            )}
-          </div>
-          <div className="mr-3%">
-            {Media(
-              "The Ukrainian government is to launch a non-fungible token marking the history of the Russian invasion with unique digital art, in its latest use of digital assets to fund its war efforts.",
-              "The Guardian",
-              "https://www.theguardian.com/world/2022/mar/13/ukraine-nft-history-of-russian-invasion-war"
-            )}
-          </div>
+      <div className="mt-20px grid tablet:gap-48px mobile:gap-16px auto-rows-max tablet:grid-cols-2 laptop:grid-cols-4 mobile:grid-cols-1 font-rlight">
+        <div>
+          {Media(
+            "Ukraine's government has said it will issue non-fungible tokens (NFTs) to fund its military as they defend the country against the Russian invasion.",
+            "BBC",
+            "https://www.bbc.co.uk/news/business-60613613"
+          )}
         </div>
-        <div className="tablet:flex laptop:flex-row tablet:flex-col">
-          <div className="mr-3%">
-            {Media(
-              "After careful consideration we decided to cancel airdrop,” Mr Fedorov tweeted … “Instead, we will announce NFTs to support Ukrainian Armed Forces soon. We DO NOT HAVE any plans to issue any fungible tokens.",
-              "The Independent",
-              "https://www.independent.co.uk/tech/ukraine-nft-russia-crypto-airdrop-war-b2027648.html"
-            )}
-          </div>
-          <div className="mr-3%">
-            {Media(
-              "Ukraine plans to become the first developed country to issue its own collection of non-fungible tokens, as it looks to capitalise on a flood of crypto donations to back its war against Russia.",
-              "Financial Times",
-              "https://www.ft.com/content/b4e13435-a818-4d80-94a4-4149a702a094"
-            )}
-          </div>
+        <div>
+          {Media(
+            "The Ukrainian government is to launch a non-fungible token marking the history of the Russian invasion with unique digital art, in its latest use of digital assets to fund its war efforts.",
+            "The Guardian",
+            "https://www.theguardian.com/world/2022/mar/13/ukraine-nft-history-of-russian-invasion-war"
+          )}
+        </div>
+        <div>
+          {Media(
+            "After careful consideration we decided to cancel airdrop,” Mr Fedorov tweeted … “Instead, we will announce NFTs to support Ukrainian Armed Forces soon. We DO NOT HAVE any plans to issue any fungible tokens.",
+            "The Independent",
+            "https://www.independent.co.uk/tech/ukraine-nft-russia-crypto-airdrop-war-b2027648.html"
+          )}
+        </div>
+        <div>
+          {Media(
+            "Ukraine plans to become the first developed country to issue its own collection of non-fungible tokens, as it looks to capitalise on a flood of crypto donations to back its war against Russia.",
+            "Financial Times",
+            "https://www.ft.com/content/b4e13435-a818-4d80-94a4-4149a702a094"
+          )}
         </div>
       </div>
     </div>
