3a6ca8b7c722be8660d6579cfed4a51a4e146f4f
Danylo Mysak <danmysak@gmail.com>
2022-04-25 15:12:06 +0300

    Breadcrumbs for the auction listing

--- pages/auction/index.tsx
+++ pages/auction/index.tsx
@@ -6,7 +6,18 @@ import type { NextPage } from 'next';
 const AuctionPage: NextPage<SharedProps> = (props) => {
   return (
     <>
-      <PageHead title="Auction - Meta History: Museum of War" />
+      <PageHead
+          title="Auction - Meta History: Museum of War"
+          data={{
+              '@context': 'https://schema.org',
+              '@type': 'BreadcrumbList',
+              itemListElement: [{
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: 'Auction',
+              }]
+          }}
+      />
       <Auction
         signerAddress={props.signerAddress}
         handleConnect={props.handleConnect}
