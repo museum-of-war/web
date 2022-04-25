f7f41c5f7fb001597d391bcd5c52952a5d0e43c4
Danylo Mysak <danmysak@gmail.com>
2022-04-25 15:13:48 +0300

    Breadcrumbs for the warline listing

--- pages/warline.tsx
+++ pages/warline.tsx
@@ -5,7 +5,18 @@ import Warline from '@sections/Warline/Warline';
 
 const WarlinePage: NextPage<SharedProps> = () => (
   <>
-    <PageHead title="Warline - Meta History: Museum of War" />
+    <PageHead
+        title="Warline - Meta History: Museum of War"
+        data={{
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: [{
+                '@type': 'ListItem',
+                position: 1,
+                name: 'Warline',
+            }]
+        }}
+    />
     <Warline />
   </>
 );
