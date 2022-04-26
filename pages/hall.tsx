23cb0fa24d9ac2184b4707f3b9729468a14285a8
Danylo Mysak <danmysak@gmail.com>
2022-04-26 16:15:35 +0300

    Meta data for The Hall

--- pages/hall.tsx
+++ pages/hall.tsx
@@ -1,16 +1,24 @@
+import PageHead from '@components/PageHead';
 import { SharedProps } from '@components/wrapper';
 import TheHall from '@sections/TheHall/TheHall';
-import Head from 'next/head';
 import type { NextPage } from 'next';
 
 const TheHallPage: NextPage<SharedProps> = () => {
   return (
     <>
-      <Head>
-        <title>The Hall of Meta History</title>
-        <meta name="description" content="The Hall" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <PageHead
+          title="The Hall"
+          description="Help Ukraine by bidding on war-related art created by those affected."
+          data={{
+              '@context': 'https://schema.org',
+              '@type': 'BreadcrumbList',
+              itemListElement: [{
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: 'The Hall',
+              }]
+          }}
+      />
       <TheHall />
     </>
   );
