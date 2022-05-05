e76d6082087a164b5dacbd2e345142f8d77e5fec
Danylo Mysak <danmysak@gmail.com>
2022-05-05 13:39:49 +0300

    Taking container width into account when deciding on the size of scaled images

--- components/ScaledImage.tsx
+++ components/ScaledImage.tsx
@@ -14,8 +14,16 @@ type ScaledImageProps = {
   laptop?: number;
 };
 
-function ratioToViewport(ratio?: number): string {
-  return `${Math.round(100 * (ratio ?? 1))}vw`;
+function applyRatio(ratio: number | undefined, value: string): string {
+  const match = value.match(/^(?<number>\d+)(?<unit>[a-z]+)$/);
+  if (!match) {
+    throw new Error(`"${value}" is not in the format NUMBER + UNIT as expected`);
+  }
+  return Math.round(Number(match.groups!.number) * (ratio ?? 1)) + match.groups!.unit!;
+}
+
+function ratioToViewport(ratio: number | undefined): string {
+  return applyRatio(ratio, '100vw');
 }
 
 function ScaledImage({
@@ -43,7 +51,11 @@ function ScaledImage({
             src={src}
             alt={alt ?? ''}
             layout="responsive"
-            sizes={`(min-width: ${screens.laptop}) ${ratioToViewport(laptop)}, ${ratioToViewport(mobile)}`}
+            sizes={`
+              (min-width: ${screens.desktop}) ${applyRatio(laptop, screens.desktop)},
+              (min-width: ${screens.laptop}) ${ratioToViewport(laptop)},
+              ${ratioToViewport(mobile)}
+            `}
             width={width}
             height={height}
             onLoadingComplete={
