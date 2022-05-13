5576b5f979d9c053dd1119c8355940d4ce07dd31
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-05-13 15:11:02 +0300

    Issue 78

--- hooks/useViewport.ts
+++ hooks/useViewport.ts
@@ -2,10 +2,10 @@ import { useMediaQuery } from '@mui/material';
 
 export const useViewPort = () => {
   const isMobile = useMediaQuery('(max-width:680px)', { noSsr: true });
-  const isTablet = useMediaQuery('(min-width:680px)', {
+  const isTablet = useMediaQuery('(min-width:680px) and (max-width:1024px)', {
     noSsr: true,
   });
-  const isLaptop = useMediaQuery('(min-width:1024px)', {
+  const isLaptop = useMediaQuery('(min-width:1024px) and (max-width:1500px)', {
     noSsr: true,
   });
   const isDesktop = useMediaQuery('(min-width:1500px)', { noSsr: true });
