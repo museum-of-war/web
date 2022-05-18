3d94eca0164aed2af4e17ba29dcfd90a8cbda250
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-05-18 22:24:43 +0300

    WIP

--- sections/AboutProject/Blurb.tsx
+++ sections/AboutProject/Blurb.tsx
@@ -4,20 +4,42 @@ type PropsBlurb = {
   header: string;
   english?: string;
   ukrainian?: string;
+  isDark?: boolean;
+  classNames?: string;
+  rightContent?: React.ReactNode;
 };
 
-const Blurb = ({ header, english, ukrainian }: PropsBlurb) => {
+const Blurb = ({
+  header,
+  english,
+  ukrainian,
+  isDark,
+  classNames,
+  rightContent,
+}: PropsBlurb) => {
   return (
-    <div>
-      <p className="font-rblack mobile:text-38px mobile:leading-40px tablet:text-70px tablet:leading-72px uppercase">
-        {header}
+    <div className={classNames}>
+      <p
+        className={`font-rblack mobile:text-38px mobile:leading-40px tablet:text-70px tablet:leading-72px uppercase flex items-center flex-row ${
+          isDark ? 'text-white' : ''
+        }`}
+      >
+        {header} {rightContent || null}
       </p>
-      <div className="h-5px w-100% bg-carbon dark:bg-white" />
+      <div
+        className={`h-5px w-100% bg-carbon dark:bg-white ${
+          isDark ? 'bg-white' : ''
+        }`}
+      />
       <div className="pt-20px relative tablet:flex tablet:flex-row font-rnarrow mobile:leading-20px tablet:leading-24px mobile:text-14px tablet:text-16px tablet:justify-between">
         {english && (
-          <p className="pt-10 tablet:w-45% mobile:mb-6%">{english}</p>
+          <p className="whitespace-pre-wrap pt-10 tablet:w-45% mobile:mb-6%">
+            {english}
+          </p>
         )}
-        {ukrainian && <p className="tablet:w-45%">{ukrainian}</p>}
+        {ukrainian && (
+          <p className="whitespace-pre-wrap tablet:w-45%">{ukrainian}</p>
+        )}
       </div>
     </div>
   );
