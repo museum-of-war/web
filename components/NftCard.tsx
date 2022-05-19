a650f974503d164b35d68c9f31f5940f37e3a103
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-05-19 15:22:52 +0300

    use links for auction items

--- components/NftCard.tsx
+++ components/NftCard.tsx
@@ -1,4 +1,4 @@
-import { useAppRouter } from '@hooks/useAppRouter';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { AuctionCollectionType, AuctionItemType } from '@sections/types';
 import { calculateTimeLeft } from '@sections/AboutProject/ContentTop/CountdownBanner';
@@ -37,7 +37,6 @@ function NftCard({
     'contractAddress' | 'endsIn' | 'startsAt' | 'version'
   > &
   NftCardProps) {
-  const { push } = useAppRouter();
   const { getAuctionInfo } = useWeb3Modal();
   const { isTouch } = useViewPort();
   const [timeLeft, setTimeLeft] = useState(
@@ -50,8 +49,6 @@ function NftCard({
     bid: string;
     nextMinBid: string;
   }>({ bid: '0', nextMinBid: '' });
-
-  const navlinkToNft = () => push(`/auction/${index}`);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -74,9 +71,10 @@ function NftCard({
   }, [contractAddress, tokenId]);
 
   return (
-    <div
-      onClick={navlinkToNft}
-      className={`
+    <Link href={`/auction/${index}`} passHref>
+      <a>
+        <div
+          className={`
         mobile:my-15px tablet:my-0 desktop:my-0 mobile:border-0 tablet:border-4 desktop:border-4 border-carbon h-full
         border-solid ${
           isTouch
@@ -84,58 +82,62 @@ function NftCard({
             : 'hover:border-4 hover:border-white hover:border-solid hover:cursor-pointer'
         }
        `}
-    >
-      <div className="flex justify-center relative">
-        <ScaledImage
-          alt={name}
-          src={imageSrc}
-          postLoad={imageSrc.endsWith('.gif') || animationSrc}
-          className={`${
-            (orderIndex! < 2 || isCollection) && !type
-              ? 'desktop:h-[544px]'
-              : 'desktop:h-[240px]'
-          } ${!orderIndex && !type ? 'tablet:h-[624px]' : 'tablet:h-[288px]'}
-          mobile:h-[270px] object-contain`}
-          breakpoints={breakpoints}
-        />
-      </div>
-      <div className="p-10px">
-        <h3 className="font-rblack text-20px leading-[240%]">{name}</h3>
-        <div className="flex justify-between">
-          {!isSold && isStarted && +currentBid.bid > 0 && (
-            <div>
-              <p className="font-rlight text-12px leading-100% opacity-70">
-                {isSale ? 'Current price' : 'Current bid'}
-              </p>
-              <p className="font-rlight tablet:text-16px mobile:text-14px leading-150%">
-                {currentBid.bid} ETH
-              </p>
-            </div>
-          )}
-          {timeLeft.isLeft && (!isStarted || !isSold) && (
-            <div
+        >
+          <div className="flex justify-center relative">
+            <ScaledImage
+              alt={name}
+              src={imageSrc}
+              postLoad={imageSrc.endsWith('.gif') || animationSrc}
               className={`${
-                timeLeft.days ? '' : 'w-[100px] whitespace-nowrap'
-              }`}
-            >
-              <p className="font-rlight text-12px leading-100% opacity-70 ">
-                {isStarted ? 'Ends in' : 'Starts in'}
-              </p>
-              <p className="font-rlight tablet:text-16px mobile:text-14px leading-150% w-100%">
-                {timeLeft.days
-                  ? `${timeLeft.days} days`
-                  : `${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
-              </p>
+                (orderIndex! < 2 || isCollection) && !type
+                  ? 'desktop:h-[544px]'
+                  : 'desktop:h-[240px]'
+              } ${
+                !orderIndex && !type ? 'tablet:h-[624px]' : 'tablet:h-[288px]'
+              }
+          mobile:h-[270px] object-contain`}
+              breakpoints={breakpoints}
+            />
+          </div>
+          <div className="p-10px">
+            <h3 className="font-rblack text-20px leading-[240%]">{name}</h3>
+            <div className="flex justify-between">
+              {!isSold && isStarted && +currentBid.bid > 0 && (
+                <div>
+                  <p className="font-rlight text-12px leading-100% opacity-70">
+                    {isSale ? 'Current price' : 'Current bid'}
+                  </p>
+                  <p className="font-rlight tablet:text-16px mobile:text-14px leading-150%">
+                    {currentBid.bid} ETH
+                  </p>
+                </div>
+              )}
+              {timeLeft.isLeft && (!isStarted || !isSold) && (
+                <div
+                  className={`${
+                    timeLeft.days ? '' : 'w-[100px] whitespace-nowrap'
+                  }`}
+                >
+                  <p className="font-rlight text-12px leading-100% opacity-70 ">
+                    {isStarted ? 'Ends in' : 'Starts in'}
+                  </p>
+                  <p className="font-rlight tablet:text-16px mobile:text-14px leading-150% w-100%">
+                    {timeLeft.days
+                      ? `${timeLeft.days} days`
+                      : `${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
+                  </p>
+                </div>
+              )}
+              {isSold && isStarted && (
+                <p className="font-rlight text-12px leading-100% opacity-70 ">
+                  Sold
+                </p>
+              )}
             </div>
-          )}
-          {isSold && isStarted && (
-            <p className="font-rlight text-12px leading-100% opacity-70 ">
-              Sold
-            </p>
-          )}
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 }
 
