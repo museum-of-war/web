d178938ddbf29368010e93071ab439c64a6a375c
Danylo Mysak <danmysak@gmail.com>
2022-05-30 17:24:08 +0300

    Preventing state update on an unmounted component

---
+++ hooks/useIsMounted.ts
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from 'react';
+
+export const useIsMounted = () => {
+  const mounted = useRef(true);
+  useEffect(() => () => {
+    mounted.current = false;
+  });
+  return mounted;
+};
