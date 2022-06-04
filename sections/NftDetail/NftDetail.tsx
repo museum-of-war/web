f66764841ec7de7961a8c82f83769beb1824a270
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-06-04 17:06:04 +0300

    Fixed link

--- sections/NftDetail/NftDetail.tsx
+++ sections/NftDetail/NftDetail.tsx
@@ -5,11 +5,12 @@ import AuctionData from '@sections/Auction/AuctionData';
 import NftCardDetail from './NftCardDetail';
 import { useMemo } from 'react';
 import AuctionCollectionData from '@sections/Auction/AuctionCollectionData';
+import Link from 'next/link';
 
 type NftDetailProps = {};
 
 const NftDetail = ({}: NftDetailProps) => {
-  const { push, query } = useAppRouter();
+  const { query } = useAppRouter();
 
   const item = useMemo(() => AuctionData[Number(query.id)], [query.id]);
 
@@ -25,21 +26,20 @@ const NftDetail = ({}: NftDetailProps) => {
 
   if (!item) return <></>;
 
-  const handleToAuction = () => push(`/auction#${cardId}`);
-
   return (
     <div className="desktop:container mx-auto desktop:px-132px tablet:px-72px mobile:px-24px desktop:mb-120px tablet:mb-96px tablet:mt-0 desktop:mt-[0px]">
-      <div
-        className="flex content-center hover:cursor-pointer mb-24px"
-        onClick={handleToAuction}
-      >
-        <span className="rotate-[270deg] mr-15px">
-          <ArrowSvg />
-        </span>
-        <p className="text-14px font-rblack">Auction</p>
-      </div>
-      <Blurb header={item.name} />
-      <NftCardDetail item={item} />
+      <Link href={`/auction#${cardId}`} passHref>
+        <a>
+          <div className="flex content-center hover:cursor-pointer mb-24px">
+            <span className="rotate-[270deg] mr-15px">
+              <ArrowSvg />
+            </span>
+            <p className="text-14px font-rblack">Auction</p>
+          </div>
+          <Blurb header={item.name} />
+          <NftCardDetail item={item} />
+        </a>
+      </Link>
     </div>
   );
 };
