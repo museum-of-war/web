a4e427e697739ab802c7484c6de735186e32e876
Danylo Mysak <danmysak@gmail.com>
2022-06-04 13:25:48 +0300

    Minor fix

--- hooks/useIsClientRendered.ts
+++ hooks/useIsClientRendered.ts
@@ -1,8 +1,12 @@
 import { useLayoutEffect, useState } from 'react';
 
+// https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85?permalink_comment_id=4150784#gistcomment-4150784
+const useBrowserLayoutEffect =
+  typeof window === 'undefined' ? () => {} : useLayoutEffect;
+
 export const useIsClientRendered = () => {
   const [isClientRendered, setIsClientRendered] = useState(false);
-  useLayoutEffect(() => {
+  useBrowserLayoutEffect(() => {
     setIsClientRendered(true);
   }, []);
   return isClientRendered;
