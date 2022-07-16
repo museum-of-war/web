1d62ae8e51ddbdbde0181ce272d3e819db146f72
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-07-16 21:39:22 +0300

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactGA from 'react-ga4';
 import { AppWrapper } from '@components/wrapper';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
@@ -8,6 +9,8 @@ import { PreloaderProvider } from '@providers/PreloaderProvider';
 import { VideoProvider } from '@providers/VideoProvider';
 import '../styles/globals.css';
 
+ReactGA.initialize('G-D2QVC3VF6W');
+
 function getTheme(route: string): 'light' | 'dark' {
   return route.split('/').includes('auction') ? 'dark' : 'light';
 }
@@ -16,6 +19,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { route } = useAppRouter();
 
   const [theme, setTheme] = useState(() => getTheme(route));
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: window.location.pathname + window.location.search,
+    });
+  }, []);
 
   useEffect(() => {
     setTheme(getTheme(route));
