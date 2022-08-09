48480f33ec467b251f836a225a316348a0afaa7e
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-08-09 14:00:41 +0300

    restore team member upon request

--- sections/AboutProject/Team.tsx
+++ sections/AboutProject/Team.tsx
@@ -16,6 +16,7 @@ const Team = () => {
     { name: 'Kateryna Dranova', role: 'Voice' },
     { name: 'Rustam Abduvaliiev', role: 'Frontend' },
     { name: 'Valeria Panina', role: 'Government Communications' },
+    { name: 'Nick Zinchenko', role: 'Art Support' },
     { name: 'Alisa Fursa', role: 'Strategy' },
   ];
 
