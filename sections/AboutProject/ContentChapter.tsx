d69ce47ee4797a660d0e9e9ba7e63d41a59e5980
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-08-24 17:14:15 +0300

    change auction poster

--- sections/AboutProject/ContentChapter.tsx
+++ sections/AboutProject/ContentChapter.tsx
@@ -41,22 +41,21 @@ const ContentChapter = ({}: ContentChapterProps) => (
       </Link>
     </div>
     <div className="desktop:w-[544px] tablet:w-45% mobile:w-full mt-30px tablet:mt-0 flex flex-col">
-      <Link href="/auction/collection/avatars" passHref>
+      <Link href="/auction/collection/revival" passHref>
         <a>
           <div className="hover:cursor-pointer">
             <div className="border-4 border-carbon border-solid flex-1 flex flex-col h-full">
               <div className="desktop:p-48px tablet:py-48px tablet:px-36px mobile:py-20px mobile:px-24px flex-1 flex flex-col text-white bg-carbon">
                 <h2 className="tablet:text-32px mobile:text-27px font-rblack">
-                  Avatars for Ukraine
+                  The Revival Project
                 </h2>
                 <p className="tablet:text-16px mobile:text-14px font-normal leading-150% font-rnarrow tablet:mt-24px mobile:mt-10px tracking-wider">
-                  A charity collection of the iconic digital art created in
-                  response to the largest war since WWII
+                  Get thematic NFT artworks and contribute to Ukraine’s culture
                 </p>
               </div>
               <div className="flex flex-wrap  border-t-4 border-carbon border-solid ">
                 <ScaledImage
-                  src={`${IMG_STORAGE}/auction.gif`}
+                  src={`${IMG_STORAGE}/posters/revival.png`}
                   postLoad={true}
                   alt="Auction"
                   className="w-full"
