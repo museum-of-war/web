d103428cce2a62075450f539be99405f83f7f331
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-09-14 23:06:57 +0300

    fix import

--- pages/collection/warline.tsx
+++ pages/collection/warline.tsx
@@ -2,7 +2,7 @@ import { TITLES } from '@sections/constants';
 import PageHead from '@components/PageHead';
 import { SharedProps } from '@components/wrapper';
 import type { NextPage } from 'next';
-import Warline from '@components/../../sections/collections/Warline/Warline';
+import Warline from '@sections/Collections/Warline/Warline';
 
 const WarlinePage: NextPage<SharedProps> = () => (
   <>
