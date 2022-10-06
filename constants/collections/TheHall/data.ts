9960bed5c428a27148e24311bca4dfcd6c36453b
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-10-06 18:47:28 +0300

    The Hall updates

--- constants/collections/TheHall/data.ts
+++ constants/collections/TheHall/data.ts
@@ -27,22 +27,3 @@ export const data: HallItemType[] = [
     DescriptionUkrainian: 'Todo',
   },
 ];
-
-// {
-//   Tokenid: '1',
-//     DayNo: '1',
-//   FullDate: '2/24/2022',
-//   Time: '05:45',
-//   TwitterUrl: 'https://twitter.com/BBCNews/status/1496692738024161282',
-//   TwitterUsername: 'BBCNews',
-//   Headline:
-//   'Ukraine conflict: Russia announces special military operation in Donbas',
-//     DescriptionEnglish:
-//   "Russian forces have launched a major assault on Ukraine, firing missiles on cities and military targets.\nThe invasion by land, air and sea began after a pre-dawn TV address where Russian President Vladimir Putin demanded that Ukraine's military lay down its arms.",
-//     DescriptionUkrainian:
-//   "Російські війська розпочали масштабний наступ на Україну, обстрілюючи міста та військові об'єкти.\nВторгнення на суші, повітрі та морі почалося після передсвітанкового телезвернення, де президент Росії Володимир Путін вимагав від українських військових скласти зброю.",
-//     ArtistName: 'Yaroslav DRZK',
-//   ArtistLink: 'https://instagram.com/drzk_artist?utm_medium=copy_link',
-//   ImageType: 'drop1/1.gif',
-//   Editions: 1,
-// },
