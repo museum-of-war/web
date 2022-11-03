8f69fb3e170c23fa45e99f2d3fbf64d16224b53b
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-11-03 23:23:04 +0200

    fix fetch error

--- sections/ForArtists/ForArtists.tsx
+++ sections/ForArtists/ForArtists.tsx
@@ -87,7 +87,8 @@ export const ForArtists: React.FC = () => {
         });
         alert('Your data has been successfully saved!\nThank you!');
       } catch (e) {
-        alert(`An error occurred: ${(e as Error)?.message ?? e}`);
+        if ((e as Error)?.message !== 'Failed to fetch')
+          alert(`An error occurred: ${(e as Error)?.message ?? e}`);
       } finally {
         setLoading(false);
         setInputs({});
