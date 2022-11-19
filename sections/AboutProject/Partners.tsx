5fa710c609c373128f8520d8063282661663c097
Oleh Zasadnyy <oleh.zasadnyy@bolt.eu>
2022-11-19 23:09:26 +0000

    Update partners block

--- sections/AboutProject/Partners.tsx
+++ sections/AboutProject/Partners.tsx
@@ -1,227 +1,137 @@
-import InfiniteLoop from '@components/InfiniteLoop';
 import React from 'react';
-import { useViewPort } from '@hooks/useViewport';
 import Blurb from '@sections/AboutProject/Blurb';
 
+interface Partner {
+  src: string;
+  alt: string;
+  link: string;
+}
+
+const partners: Partner[] = [
+  {
+    src: '/img/partners/Logo_Ministry_Digital.png',
+    alt: 'Ministry of Digital Transformation',
+    link: 'https://thedigital.gov.ua/',
+  },
+  {
+    src: '/img/partners/fair.png',
+    alt: 'FAIR',
+    link: 'https://fair.xyz/',
+  },
+  {
+    src: '/img/partners/market_across.png',
+    alt: 'MarketAcross',
+    link: 'https://marketacross.com/',
+  },
+  {
+    src: '/img/partners/party_space.png',
+    alt: 'Party.Space',
+    link: 'https://www.party.space/',
+  },
+  {
+    src: '/img/partners/hacken.png',
+    alt: 'Hacken',
+    link: 'https://hackenfoundation.com/',
+  },
+  {
+    src: '/img/partners/kuna.png',
+    alt: 'Kuna',
+    link: 'https://kuna.io/',
+  },
+  {
+    src: '/img/partners/blockchain_association.png',
+    alt: 'Blockchain Association',
+    link: 'https://bau.ai/',
+  },
+  {
+    src: '/img/partners/projector.png',
+    alt: 'Projector',
+    link: 'https://prjctr.com/',
+  },
+  {
+    src: '/img/partners/disbalancer.png',
+    alt: 'Disbalancer',
+    link: 'https://disbalancer.com/',
+  },
+  {
+    src: '/img/partners/financevote.png',
+    alt: 'Finance.Vote',
+    link: 'https://finance.vote/',
+  },
+  {
+    src: '/img/partners/Logo_of_Ministry_of_Culture_of_Ukraine.png',
+    alt: 'Ministry of Culture of Ukraine',
+    link: 'https://mkip.gov.ua/',
+  },
+  {
+    src: '/img/partners/lliwell.png',
+    alt: 'LLIWELL',
+    link: 'https://www.instagram.com/lliwell/', // website is not working: https://lliwell.com/
+  },
+  {
+    src: '/img/partners/NFT_News_Today.png',
+    alt: 'NFT News Today',
+    link: 'https://nftnewstoday.com/',
+  },
+  {
+    src: '/img/partners/nevgamovni.png',
+    alt: 'Невгамовні',
+    link: 'https://nevgamovni.agency/',
+  },
+  {
+    src: '/img/partners/army_inform.png',
+    alt: 'АрміяInform',
+    link: 'https://armyinform.com.ua/',
+  },
+  {
+    src: '/img/partners/lobby_x.png',
+    alt: 'Lobby X',
+    link: 'https://thelobbyx.com/',
+  },
+  {
+    src: '/img/partners/bitcoin_dot_com.png',
+    alt: 'Bitcoin.com',
+    link: 'https://www.bitcoin.com/',
+  },
+  {
+    src: '/img/partners/School_of_Visual_Communication.png',
+    alt: 'School of Visual Communication',
+    link: 'https://svc.com.ua/',
+  },
+  {
+    src: '/img/partners/superheroes.png',
+    alt: 'Superheroes.',
+    link: 'https://superheroes.ua/',
+  },
+  {
+    src: '/img/partners/nft_calendar.png',
+    alt: 'NFT Calendar',
+    link: 'https://nftcalendar.io/',
+  },
+];
+
 const Partners = () => {
-  const { isMobile } = useViewPort();
-
-  const partners = [
-    {
-      src: '/img/partners/Logo_Ministry_Digital.png',
-      alt: 'Ministry of Digital Transformation',
-    },
-    {
-      src: '/img/partners/fair.png',
-      alt: 'FAIR',
-    },
-    {
-      src: '/img/partners/market_across.png',
-      alt: 'MarketAcross',
-    },
-    {
-      src: '/img/partners/party_space.png',
-      alt: 'Party.Space',
-    },
-    {
-      src: '/img/partners/hacken.png',
-      alt: 'Hacken',
-    },
-    {
-      src: '/img/partners/kuna.png',
-      alt: 'Kuna',
-    },
-    {
-      src: '/img/partners/blockchain_association.png',
-      alt: 'Blockchain Association',
-    },
-    {
-      src: '/img/partners/projector.png',
-      alt: 'Projector',
-    },
-    {
-      src: '/img/partners/disbalancer.png',
-      alt: 'Disbalancer',
-    },
-    {
-      src: '/img/partners/financevote.png',
-      alt: 'Finance.Vote',
-    },
-    {
-      src: '/img/partners/Logo_of_Ministry_of_Culture_of_Ukraine.png',
-      alt: 'Ministry of Culture of Ukraine',
-    },
-    /*
-    {
-      src: '/img/partners/vector.png',
-      alt: 'Vector',
-    },
-    */
-    {
-      src: '/img/partners/lliwell.png',
-      alt: 'LLIWELL',
-    },
-    {
-      src: '/img/partners/NFT_News_Today.png',
-      alt: 'NFT News Today',
-    },
-    {
-      src: '/img/partners/nevgamovni.png',
-      alt: 'Невгамовні',
-    },
-    {
-      src: '/img/partners/army_inform.png',
-      alt: 'АрміяInform',
-    },
-    /*
-    {
-      src: '/img/partners/molfar.png',
-      alt: 'Molfar',
-    },
-     */
-    {
-      src: '/img/partners/lobby_x.png',
-      alt: 'Lobby X',
-    },
-    {
-      src: '/img/partners/bitcoin_dot_com.png',
-      alt: 'Bitcoin.com',
-    },
-    {
-      src: '/img/partners/School_of_Visual_Communication.png',
-      alt: 'School of Visual Communication',
-    },
-    {
-      src: '/img/partners/superheroes.png',
-      alt: 'Superheroes.',
-    },
-    {
-      src: '/img/partners/nft_calendar.png',
-      alt: 'NFT Calendar',
-    },
-  ];
-
-  const partnersSmall = [
-    {
-      src: '/img/partners/small/Ministry_of_Digital_small.png',
-      alt: 'Ministry of Digital Transformation',
-    },
-    {
-      src: '/img/partners/small/fair_small.png',
-      alt: 'FAIR',
-    },
-    {
-      src: '/img/partners/small/market_across_small.png',
-      alt: 'MarketAcross',
-    },
-    {
-      src: '/img/partners/small/party_space_small.png',
-      alt: 'Party.Space',
-    },
-    {
-      src: '/img/partners/small/hacken_small.png',
-      alt: 'Hacken',
-    },
-    {
-      src: '/img/partners/small/kuna_small.png',
-      alt: 'Kuna',
-    },
-    {
-      src: '/img/partners/small/blockchain_association_small.png',
-      alt: 'Blockchain Association',
-    },
-    {
-      src: '/img/partners/small/projector_small.png',
-      alt: 'Projector',
-    },
-    {
-      src: '/img/partners/small/disbalancer_small.png',
-      alt: 'Disbalancer',
-    },
-    {
-      src: '/img/partners/small/financevote_small.png',
-      alt: 'Finance.Vote',
-    },
-    {
-      src: '/img/partners/small/Logo_of_Ministry_of_Culture_of_Ukraine_small.png',
-      alt: 'Ministry of Culture of Ukraine',
-    },
-    /*
-    {
-      src: '/img/partners/small/vector_small.png',
-      alt: 'Vector',
-    },
-    */
-    {
-      src: '/img/partners/small/lliwell_small.png',
-      alt: 'LLIWELL',
-    },
-    {
-      src: '/img/partners/small/NFT_News_Today_small.png',
-      alt: 'NFT News Today',
-    },
-    {
-      src: '/img/partners/small/nevgamovni_small.png',
-      alt: 'Невгамовні',
-    },
-    {
-      src: '/img/partners/small/army_inform_small.png',
-      alt: 'АрміяInform',
-    },
-    /*
-    {
-      src: '/img/partners/small/molfar_small.png',
-      alt: 'Molfar',
-    },
-     */
-    {
-      src: '/img/partners/small/lobby_x_small.png',
-      alt: 'Lobby X',
-    },
-    {
-      src: '/img/partners/small/bitcoin_dot_com_small.png',
-      alt: 'Bitcoin.com',
-    },
-    {
-      src: '/img/partners/small/School_of_Visual_Communication_small.png',
-      alt: 'School of Visual Communication',
-    },
-    {
-      src: '/img/partners/small/superheroes_small.png',
-      alt: 'Superheroes.',
-    },
-    {
-      src: '/img/partners/small/nft_calendar_small.png',
-      alt: 'NFT Calendar',
-    },
-  ];
-
   return (
     <div className="desktop:py-120px tablet:py-[96px] mobile:py-60px">
       <Blurb header="Partners" />
-      {isMobile ? (
-        <InfiniteLoop speed={partnersSmall.length * 2.5} className={'h-80px'}>
-          {partnersSmall.map((partner, idx) => (
+      <div className="py-24px tablet:py-40px grid gap-x-32px gap-y-40px tablet:gap-x-56px tablet:gap-y-64px grid-cols-2 tablet:grid-cols-3 new_md:grid-cols-4 desktop:grid-cols-4 items-center">
+        {partners.map((partner, idx) => (
+          <a
+            key={idx}
+            className="new_md:grayscale new_md:hover:grayscale-0 transition-all duration-300 ease-in"
+            href={partner.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
-              className="mr-80px"
-              key={idx}
+              className="max-h-[80px]"
               src={partner.src}
               alt={partner.alt}
+              loading="lazy"
             />
-          ))}
-        </InfiniteLoop>
-      ) : (
-        <InfiniteLoop speed={partners.length * 2.5} className={'h-192px'}>
-          {partners.map((partner, idx) => (
-            <img
-              className="mr-144px"
-              key={idx}
-              src={partner.src}
-              alt={partner.alt}
-            />
-          ))}
-        </InfiniteLoop>
-      )}
+          </a>
+        ))}
+      </div>
       <div className="h-5px w-100% tablet:mb-16px mobile:mb-12px bg-carbon dark:bg-white" />
     </div>
   );
